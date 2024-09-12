575292508b6b8f3128e5e5b65fc7cc3fc274983e
arpitpatnicompose <arpit@compose.co.in>
2024-09-12 14:50:23 +0530

    Channel names hidden

--- src/components/entry-point/entry-point.tsx
+++ src/components/entry-point/entry-point.tsx
@@ -9,9 +9,9 @@ import TinyEditor from '../Tinymce';
 // Here we split up the main (app) bundle with the actual application business logic.
 // Splitting by route is usually recommended and you can potentially have a splitting
 // point for each route. More info at https://reactjs.org/docs/code-splitting.html
-const AsyncApplicationRoutes = lazy(
-  () => import('../../routes' /* webpackChunkName: "routes" */)
-);
+// const AsyncApplicationRoutes = lazy(
+//   () => import('../../routes' /* webpackChunkName: "routes" */)
+// );
 
 // Ensure to setup the global error listener before any React component renders
 // in order to catch possible errors on rendering/mounting.
@@ -20,7 +20,7 @@ setupGlobalErrorListener();
 const EntryPoint = () => (
   <CustomViewShell enableReactStrictMode applicationMessages={loadMessages}>
     <TinyEditor/>
-    <AsyncApplicationRoutes />
+    {/* <AsyncApplicationRoutes /> */}
   </CustomViewShell>
 );
 
